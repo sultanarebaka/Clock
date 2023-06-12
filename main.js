1fb1d3a75c26c7f8de9dc676e77ea7288b29207e
sultanarebaka <117830026+sultanarebaka@users.noreply.github.com>
2023-06-12 15:10:03 +0600

    Add files via upload

--- main.js
+++
@@ -1,6 +0,0 @@
-const secondHand = document.querySelector('.second-hand');
-  const minsHand = document.querySelector('.min-hand');
-  const hourHand = document.querySelector('.hour-hand');
-
-  function setDate() {
-    const now = new Date();

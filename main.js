a1abd0426317fc298d0184683baae74e7edcf1d0
sultanarebaka <117830026+sultanarebaka@users.noreply.github.com>
2023-06-13 22:14:08 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+const hourHand = document.querySelector('[data-hour-hand]');
+const minuteHand = document.querySelector('[data-minute-hand]');
+const secondHand = document.querySelector('[data-second-hand]');
+

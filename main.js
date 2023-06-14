34fe10e1cd464518e392a4f2666a50404c19c19e
sultanarebaka <117830026+sultanarebaka@users.noreply.github.com>
2023-06-14 15:44:10 +0600

    Add files via upload

--- main.js
+++ main.js
@@ -4,4 +4,8 @@ const minuteHand = document.querySelector('[data-minute-hand]')
 const secondHand = document.querySelector('[data-second-hand]')
 
 function setClock() {
- 
+    const currentDate = new Date()
+    const secondsRatio = currentDate.getSeconds() / 60
+    const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60
+    const hoursRatio = (minutesRatio + currentDate.getHours()) / 12
+}  
